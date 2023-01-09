ea488fc6c1b263ae5d7a6bcff21d489e5782eb13
jeon9897 <jeon9897@naver.com>
2023-01-09 15:11:12 +0900

    jquery

--- 0105/script/movie_gallery.js
+++ 0105/script/movie_gallery.js
@@ -11,11 +11,18 @@ img_list.forEach((el, index) => {
   el.onclick=()=>{
     console.log(index);
     n = index+1; //인덱스번호에 1을 더하여 1부터 숫자가 나오게...
-
-    document.getElementById('page').innerHTML=n + ' / 16';
-    document.getElementById('photo').src='./images/movie_image'+n+'.jpg';
+    img_change();    
   }
 });
+
+function img_change(){
+  for(let j=0;j<img_list.length;j++){
+    img_list[j].style.border='none';
+  }
+    document.getElementById('page').innerHTML=n + ' / 16';
+    document.getElementById('photo').src='./images/movie_image'+n+'.jpg';
+    img_list[n-1].style.border='3px solid #f00';
+}
 
 left_btn.addEventListener('click', function(){
   if(n == 1){
@@ -24,8 +31,7 @@ left_btn.addEventListener('click', function(){
     n--;
   }
   console.log(n); //확인한다.
-  document.getElementById('page').innerHTML=n + ' / 16';
-  document.getElementById('photo').src='./images/movie_image'+n+'.jpg';
+  img_change();
 });
 
 right_btn.addEventListener('click', function(){
@@ -35,6 +41,5 @@ right_btn.addEventListener('click', function(){
     n++;
   }
   console.log(n); //확인한다.
-  document.getElementById('page').innerHTML=n + ' / 16';
-  document.getElementById('photo').src='./images/movie_image'+n+'.jpg';
+  img_change();
 });
